bc9107164dc3282cf56c647856318912d4124934
weiraneve <907221539@qq.com>
2025-02-07 19:06:33 +0800

    feat: add hero index api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HeroController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello world';
 });
+
+Route::get('/heroes', [HeroController::class, 'index']);
